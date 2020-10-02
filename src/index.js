f7ca9a589e9deae29c5a83ee913e2af73b0d1afd
Artem Karelin <AKarelin@ibagroup.eu>
2020-10-02 23:07:13 +0300

    feat: add logic

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || !array.length) {
+    return 0;
+  } else {
+    let min = array[0];
+    array.forEach(el => {
+      if (el <= min) {
+        min = el;
+      }
+    });
+    return min;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || !array.length) {
+    return 0;
+  } else {
+    let max = Math.max.apply(null, array);
+    return max;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || !array.length) {
+    return 0;
+  } else {
+      const average = array.reduce((sum, number, index, arr) => {
+        sum += number;
+        if (index === array.length - 1) {
+          return sum/array.length;
+        } else {
+          return sum;
+        }
+      });
+      return average;
+    }
 }
